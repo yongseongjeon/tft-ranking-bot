d7fc61305437855baacf7e10e8fbead885d9d67a
yongseongjeon <yongseong.jeon95@gmail.com>
2023-07-04 13:33:44 +0900

    feat: 전역변수 제거 및 firebase DB에서 관리

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import { Client, Intents, MessageEmbed } from "discord.js";
 import { parseName, sortByMmrInDescend } from "./util.js";
 import { requestSummoner } from "./request.js";
 import "dotenv/config";
+import { getValueFromDatabase, setValueToDatabase } from "./db.js";
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
-let people = [];
 client.on("ready", handleReady);
 client.on("messageCreate", handleMessage);
 client.login(process.env.DISCORD_API_KEY);
@@ -22,23 +22,26 @@ async function handleMessage(message) {
   if (!message.content) {
     return;
   }
-  const [_, keyword, name] = message.content
+  const users = await getValueFromDatabase("users");
+  const [_, keyword, enteredName] = message.content
     .trim()
     .replace(/^<@[!&]?\d+>/, "")
     .split(" ");
   if (keyword === "추가") {
-    const isAlreadyExistUser = people.includes(parseName(name));
+    const isAlreadyExistUser = users.includes(parseName(enteredName));
     if (isAlreadyExistUser) {
-      message.channel.send(`${parseName(name)}님은 이미 추가되었습니다.`);
+      message.channel.send(`${parseName(enteredName)}님은 이미 추가되었습니다.`);
       return;
     }
-    people = [...people, parseName(name)];
-    message.channel.send(`${parseName(name)}님이 추가되었습니다.`);
+    const updatedPeople = [...users, parseName(enteredName)];
+    setValueToDatabase("users", updatedPeople);
+    message.channel.send(`${parseName(enteredName)}님이 추가되었습니다.`);
     return;
   }
   if (keyword === "삭제") {
-    people = people.filter((p) => p.name !== name);
-    message.channel.send(`${parseName(name)}님이 삭제되었습니다.`);
+    const updatedPeople = users.filter((userName) => userName !== parseName(enteredName));
+    setValueToDatabase("users", updatedPeople);
+    message.channel.send(`${parseName(enteredName)}님이 삭제되었습니다.`);
     return;
   }
   const isValidKeyword = keyword === "추가" || keyword === "삭제" || keyword === "순위" || keyword === "도움말";
@@ -52,13 +55,13 @@ async function handleMessage(message) {
     );
     return;
   }
-  const hasPeople = people.length >= 1;
+  const hasPeople = users.length >= 1;
   if (keyword === "순위") {
     if (!hasPeople) {
       message.channel.send("현재 추가된 사람이 없습니다.");
       return;
     }
-    const requestPromises = people.map(requestSummoner);
+    const requestPromises = users.map(requestSummoner);
     const summoners = await Promise.all(requestPromises);
     summoners.sort(sortByMmrInDescend);
     const names = summoners.map(({ summonerName }) => summonerName);
@@ -68,7 +71,7 @@ async function handleMessage(message) {
       }
       return `${tier} ${rank} ${leaguePoints}`;
     });
-    const restRanking = Array.from({ length: Math.max(0, people.length - 3) }, (_, i) => 4 + i);
+    const restRanking = Array.from({ length: Math.max(0, users.length - 3) }, (_, i) => 4 + i);
     const table = new MessageEmbed().setTitle("WE CLAN TFT 랭킹").addFields(
       {
         name: "순위\n",
